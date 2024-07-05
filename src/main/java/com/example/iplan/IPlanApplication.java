a306909faddb46d94f9a6e1392d632e25fcdafbb
정하은 <he3080@naver.com>
2024-07-05 22:03:48 +0900

    base project

---
+++ src/main/java/com/example/iplan/IPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.iplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iplan/IPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
