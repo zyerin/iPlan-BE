7a4edba6749ce096dce852f54b8b8d03932f0c9f
zyerin <zyerin5974@gmail.com>
2024-07-10 10:06:13 +0900

    Update Dockerfile & FirebaseConfig

--- src/main/java/com/example/iplan/config/FirebaseConfig.java
+++ src/main/java/com/example/iplan/config/FirebaseConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
@@ -14,17 +15,19 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
+            // 환경 변수에서 파일 경로를 가져옴, 기본값은 Docker 컨테이너 내부 경로
             String firebaseConfigPath = System.getenv("FIREBASE_CONFIG_PATH");
-            if (firebaseConfigPath == null) {
-                firebaseConfigPath = "src/main/resources/iplan-firebase.json";
+            if (firebaseConfigPath == null || firebaseConfigPath.isEmpty()) {
+                firebaseConfigPath = "/app/serviceAccountKey.json"; // Docker 컨테이너 내부 경로
             }
 
+            // Firebase 초기화
             FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
             FirebaseApp.initializeApp(options);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
