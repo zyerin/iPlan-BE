fde093bdc5ae4a7e4a28aa2a4629436075058b22
정하은 <he3080@naver.com>
2024-07-31 15:39:11 +0900

    refact: 아마 사라질수도 있지만 Bean 주입 오류 때문에 일시적으로 Component추가

--- src/main/java/com/example/iplan/Domain/PlanChild.java
+++ src/main/java/com/example/iplan/Domain/PlanChild.java
@@ -2,12 +2,14 @@ package com.example.iplan.Domain;
 
 import com.google.cloud.firestore.annotation.DocumentId;
 import lombok.Getter;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
+@Getter
 public class PlanChild {
 
-    @Getter
     @DocumentId
     private String id; // Firestore 문서의 ID
 
