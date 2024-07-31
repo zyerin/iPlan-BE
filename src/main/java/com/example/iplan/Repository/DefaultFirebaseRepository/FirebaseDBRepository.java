8fd30eb4afb3800b5dc1880b9d273b9b0bae5945
정하은 <he3080@naver.com>
2024-07-31 13:10:25 +0900

    feat: 모든 도메인들의 repository가 상속할 DefaultFirebaseDBRepository구현

---
+++ src/main/java/com/example/iplan/Repository/DefaultFirebaseRepository/FirebaseDBRepository.java
@@ -0,0 +1,10 @@
+package com.example.iplan.Repository.DefaultFirebaseRepository;
+
+import java.util.concurrent.ExecutionException;
+
+public interface FirebaseDBRepository<T, ID> {
+    void save(T entity) throws ExecutionException, InterruptedException;
+    void update(T entity) throws ExecutionException, InterruptedException;
+    void delete(ID id) throws ExecutionException, InterruptedException;
+    T findById(ID id) throws ExecutionException, InterruptedException;
+}
