5e8865b60b34db159381b90b66bbab202941c81e
정하은 <he3080@naver.com>
2024-08-21 16:30:33 +0900

    Feat: 계획을 파이어베이스에 기록하는 Repository 생성 + 해당 날짜의 데이터들만 가져오는 findByDate 구현

--- src/main/java/com/example/iplan/Repository/PlanChildRepository.java
+++ src/main/java/com/example/iplan/Repository/PlanChildRepository.java
@@ -2,7 +2,16 @@ package com.example.iplan.Repository;
 
 import com.example.iplan.Repository.DefaultFirebaseRepository.DefaultFirebaseDBRepository;
 import com.example.iplan.Domain.PlanChild;
+import com.example.iplan.Service.PlanChildService;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Repository
 public class PlanChildRepository extends DefaultFirebaseDBRepository<PlanChild> {
@@ -10,5 +19,28 @@ public class PlanChildRepository extends DefaultFirebaseDBRepository<PlanChild> 
     public PlanChildRepository() {
         setEntityClass(PlanChild.class);
         setCollectionName("PlanChild");
+    }
+
+    public List<PlanChild> findByDate(String user_id, LocalDate targetDate) throws ExecutionException, InterruptedException {
+        Firestore firestore = FirestoreClient.getFirestore();
+        //어떤 컬렉션인지 객체 가져옴
+        CollectionReference collection = firestore.collection("PlanChild");
+
+        // 특정 날짜와 일치하는 문서들에 대해 쿼리
+        ApiFuture<QuerySnapshot> apiFutureList = collection
+                .whereEqualTo("user_id", user_id) // 사용자의 user_id로 필터링
+                .whereEqualTo("postDate", targetDate) // 특정 날짜로 필터링, targetDate는 "YYYY-MM-DD" 형식의 문자열이라고 가정
+                .get();
+
+        QuerySnapshot querySnapshot = apiFutureList.get();
+
+        List<PlanChild> plans = new ArrayList<>();
+
+        // 쿼리 결과의 모든 문서를 PlanChild 객체로 변환하여 리스트에 추가
+        for (QueryDocumentSnapshot document : querySnapshot.getDocuments()) {
+            plans.add(document.toObject(PlanChild.class));
+        }
+
+        return plans; // 일치하는 모든 PlanChild 문서를 포함하는 리스트 반환
     }
 }
