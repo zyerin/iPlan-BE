0617a0d14c3f2bf05f2b7cb0d98fba5cf33bb7b9
zyerin <zyerin5974@gmail.com>
2024-10-09 20:14:44 +0900

    Feat: 소셜 로그인 유저 정보 Entity 생성

--- src/main/java/com/example/iplan/Domain/UserSocial.java
+++ src/main/java/com/example/iplan/Domain/UserSocial.java
@@ -1,13 +1,13 @@
 package com.example.iplan.Domain;
 
-public class User {
+public class UserSocial {
     private String id; // Firebase Auth UID
     private String name;
     private String email;
 
-    public User() {}
+    public UserSocial() {}
 
-    public User(String id, String name, String email) {
+    public UserSocial(String id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
