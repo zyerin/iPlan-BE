e7209e1e6aadd3cf060a3d0cecfbfd901f245180
zyerin <zyerin5974@gmail.com>
2024-10-09 20:36:44 +0900

    Feat: 계획 카테고리를 파이어베이스에 기록하는 Repository 생성

--- src/main/java/com/example/iplan/Repository/PlanCategoryRepository.java
+++ src/main/java/com/example/iplan/Repository/PlanCategoryRepository.java
@@ -1,2 +1,14 @@
-package com.example.iplan.Repository;public class PlanCategoryRepository {
+package com.example.iplan.Repository;
+
+import com.example.iplan.Domain.PlanCategory;
+import com.example.iplan.Repository.DefaultFirebaseRepository.DefaultFirebaseDBRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PlanCategoryRepository extends DefaultFirebaseDBRepository<PlanCategory> {
+
+    public PlanCategoryRepository(){
+        setEntityClass(PlanCategory.class);
+        setCollectionName("PlanCategory");
+    }
 }
