b260dc6be5d6baa7bf43c3270cbbda842cce4cb9
zyerin <zyerin5974@gmail.com>
2024-10-09 21:06:46 +0900

    Refact: Firestore와 상호작용하는 Firestore 및 소셜 로그인을 위한 Firebase Authentication 인스턴스 생성

--- src/main/java/com/example/iplan/config/FirebaseConfig.java
+++ src/main/java/com/example/iplan/config/FirebaseConfig.java
@@ -1,28 +1,42 @@
 package com.example.iplan.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
-    @PostConstruct
-    public void firestore() throws IOException {
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/iplan-firebase.json");
 
-        FileInputStream serviceAccount =
-                new FileInputStream("/app/iplan-firebase.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
+        FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
-        FirebaseApp.initializeApp(options);
+        return FirebaseApp.initializeApp(options);
+    }
 
+    @Bean
+    public FirebaseAuth getFirebaseAuth() {
+        try {
+            return FirebaseAuth.getInstance(firebaseApp());
+        } catch (IOException e) {
+            throw new RuntimeException("Firebase initialization error", e);
+        }
+    }
+
+    @Bean
+    public Firestore firestore() throws IOException {
+        return FirestoreClient.getFirestore(firebaseApp());
     }
 }
