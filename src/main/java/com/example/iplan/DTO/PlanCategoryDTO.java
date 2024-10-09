1853d66d9eeca04291d1d0e436019e1ea6e1afc2
zyerin <zyerin5974@gmail.com>
2024-10-09 20:16:05 +0900

    Feat: 계획 카테고리 DTO 생성

--- src/main/java/com/example/iplan/DTO/PlanCategoryDTO.java
+++ src/main/java/com/example/iplan/DTO/PlanCategoryDTO.java
@@ -1,2 +1,19 @@
-package com.example.iplan.DTO;public class PlanCategoryDTO {
+package com.example.iplan.DTO;
+
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.firebase.database.annotations.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class PlanCategoryDTO {
+
+    private String id;
+    private String user_id;
+
+    @NotNull
+    private String name;
 }
