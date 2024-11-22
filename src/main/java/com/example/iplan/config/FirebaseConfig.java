ee8d7d1342b212aef4d07fe04ecfb95973416177
정하은 <he3080@naver.com>
2024-11-22 09:51:52 +0900

    comment: FirebaseConfig 각 함수 주석 설명 추가 및 함수 이름 변경

--- src/main/java/com/example/iplan/config/FirebaseConfig.java
+++ src/main/java/com/example/iplan/config/FirebaseConfig.java
@@ -15,6 +15,12 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+    /**
+     * Firebase 서비스 전반의 기본적인 초기화 역할을 한다.
+     * Firebase SDK의 모든 서비스(Auth, Firestore, Storage)를 구성하고 애플리케이션에서 사용할 수 있도록 함
+     * @return
+     * @throws IOException
+     */
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         FileInputStream serviceAccount = new FileInputStream("src/main/resources/iplan-firebase.json");
@@ -26,8 +32,14 @@ public class FirebaseConfig {
         return FirebaseApp.initializeApp(options);
     }
 
+    /**
+     * FirebaseAuth를 Spring Context에 빈으로 등록
+     * Firebase 인증 서비스를 사용하는 데 필요한 객체
+     * 사용자의 인증과 관련된 작업(로그인, 로그아웃, 토큰 검증, 사용자 관리 등) 처리
+     * @return
+     */
     @Bean
-    public FirebaseAuth getFirebaseAuth() {
+    public FirebaseAuth firebaseAuth() {
         try {
             return FirebaseAuth.getInstance(firebaseApp());
         } catch (IOException e) {
@@ -35,6 +47,11 @@ public class FirebaseConfig {
         }
     }
 
+    /**
+     * Firebase Firestore 데이터베이스에 접근하는 데 필요한 객체
+     * @return
+     * @throws IOException
+     */
     @Bean
     public Firestore firestore() throws IOException {
         return FirestoreClient.getFirestore(firebaseApp());
