3d97e51eb6f2eae0a9f1eaf7a30defa3de1c1f39
zyerin <zyerin5974@gmail.com>
2024-12-04 20:55:39 +0900

    refact: 충돌 해결

--- src/main/java/com/example/iplan/Domain/ScreenTime.java
+++ src/main/java/com/example/iplan/Domain/ScreenTime.java
@@ -1,2 +1,38 @@
-package com.example.iplan.Domain;public class ScreenTime {
+package com.example.iplan.Domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.firebase.database.annotations.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScreenTime {
+    @DocumentId
+    private String id;
+
+    @NotNull
+    private String user_id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private String date;
+
+    // 사용자가 정한 최소 측정 마감 시간
+    @JsonFormat(pattern = "HH:mm")
+    private String deadLineTime;
+
+    // 사용자가 정한 핸드폰 사용 시간
+    @JsonFormat(pattern = "HH:mm")
+    private String goalTime;
 }
