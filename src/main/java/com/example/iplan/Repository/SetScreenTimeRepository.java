0979e1767d09f8130c033a447849f2d542672b48
zyerin <zyerin5974@gmail.com>
2024-12-04 21:33:01 +0900

    feat: SetScreenTimeRepository 코드 추가: findByUserIdAndMonth

--- src/main/java/com/example/iplan/Repository/SetScreenTimeRepository.java
+++ src/main/java/com/example/iplan/Repository/SetScreenTimeRepository.java
@@ -5,11 +5,14 @@ import com.example.iplan.Repository.DefaultFirebaseRepository.DefaultFirebaseDBR
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -39,4 +42,25 @@ public class SetScreenTimeRepository extends DefaultFirebaseDBRepository<ScreenT
             return null;
         }
     }
+
+    public List<ScreenTime> findByUserIdAndMonth(String userId, LocalDate startDate, LocalDate endDate) throws ExecutionException, InterruptedException {
+        Firestore firestore = FirestoreClient.getFirestore();
+        // Firestore의 Collection 참조
+        CollectionReference collection = firestore.collection("ScreenTime");
+
+        // 날짜를 문자열로 변환 (Firestore에서 문자열 기반 비교)
+        String startDateString = startDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        String endDateString = endDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+        // 쿼리 구성
+        Query query = collection
+                .whereEqualTo("user_id", userId) // 사용자 ID 필터
+                .whereGreaterThanOrEqualTo("date", startDateString) // 시작 날짜 필터
+                .whereLessThanOrEqualTo("date", endDateString); // 종료 날짜 필터
+
+        // Firestore에서 데이터 가져오기
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+        return querySnapshot.get().toObjects(ScreenTime.class);
+    }
+
 }
--- src/main/java/com/example/iplan/Service/ScreenTimeService.java
+++
@@ -1,2 +0,0 @@
-package com.example.iplan.Service;public class ScreenTimeService {
-}
