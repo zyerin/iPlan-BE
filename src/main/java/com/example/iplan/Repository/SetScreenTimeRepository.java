9ff9b27b7401a44e7a46316d04d4892c906d15f2
정하은 <he3080@naver.com>
2024-12-04 12:06:51 +0900

    refact: - DefaultFirebaseRepository에서 firestore 선언 시점 변경 후 자식 리포지토리에서 빈주입 - findByDate 로직변경

--- src/main/java/com/example/iplan/Repository/SetScreenTimeRepository.java
+++ src/main/java/com/example/iplan/Repository/SetScreenTimeRepository.java
@@ -10,18 +10,20 @@ import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Repository
 public class SetScreenTimeRepository extends DefaultFirebaseDBRepository<ScreenTime> {
 
-    public SetScreenTimeRepository(){
+    public SetScreenTimeRepository(Firestore firestore){
+        super(firestore);
         setEntityClass(ScreenTime.class);
         setCollectionName("ScreenTime");
     }
 
-    public ScreenTime findByDate(String user_id, LocalDate targetDate) throws ExecutionException, InterruptedException{
-        Firestore firestore = FirestoreClient.getFirestore();
+    public ScreenTime findByDate(String user_id, String targetDate) throws ExecutionException, InterruptedException{
+        /*Firestore firestore = FirestoreClient.getFirestore();
 
         CollectionReference collection = firestore.collection("ScreenTime");
 
@@ -36,6 +38,11 @@ public class SetScreenTimeRepository extends DefaultFirebaseDBRepository<ScreenT
             return querySnapshot.getDocuments().get(0).toObject(ScreenTime.class);
         }else{
             return null;
-        }
+        }*/
+        Map<String, Object> filters = Map.of(
+                "user_id", user_id,
+                "date", targetDate
+        );
+        return findByFields(filters);
     }
 }
