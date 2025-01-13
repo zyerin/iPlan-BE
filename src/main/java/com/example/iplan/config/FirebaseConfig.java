11a63e69d07cd8370ecaa903347981ae1dff0c0c
정하은 <he3080@naver.com>
2025-01-13 21:20:15 +0900

    도커 환경변수 설정

--- src/main/java/com/example/iplan/config/FirebaseConfig.java
+++ src/main/java/com/example/iplan/config/FirebaseConfig.java
@@ -28,9 +28,11 @@ public class FirebaseConfig {
         // Docker 환경에서는 환경 변수에서 경로를 읽고, 로컬 환경에서는 프로퍼티에서 읽는다.
         if (System.getenv("FIREBASE_CONFIG_PATH") != null) {
             // Docker 환경
+            System.out.println("도커 환경: "+ System.getenv("FIREBASE_CONFIG_PATH"));
             configPath = System.getenv("FIREBASE_CONFIG_PATH");
         } else {
             // 로컬 환경
+            System.out.println("환경변수 null임: "+ System.getenv("FIREBASE_CONFIG_PATH"));
             configPath = "src/main/resources/iplan-firebase.json";  // 로컬 파일 경로
         }
         
