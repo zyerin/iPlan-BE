2e2de6b208fcf83aa0603e5b726246bc409821f0
정하은 <he3080@naver.com>
2025-01-13 19:26:52 +0900

    Refact: 필요없는 주석 제거

--- src/main/java/com/example/iplan/Repository/GetScreenTimeOCRRepository.java
+++ src/main/java/com/example/iplan/Repository/GetScreenTimeOCRRepository.java
@@ -2,16 +2,9 @@ package com.example.iplan.Repository;
 
 import com.example.iplan.Domain.ScreenTimeOCRResult;
 import com.example.iplan.Repository.DefaultFirebaseRepository.DefaultFirebaseDBRepository;
-import com.example.iplan.Repository.DefaultFirebaseRepository.FirebaseDBRepository;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QuerySnapshot;
-import com.google.firebase.cloud.FirestoreClient;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -25,22 +18,6 @@ public class GetScreenTimeOCRRepository extends DefaultFirebaseDBRepository<Scre
     }
 
     public ScreenTimeOCRResult findByDate(String user_id, String targetDate) throws ExecutionException, InterruptedException {
-        /*Firestore firestore = FirestoreClient.getFirestore();
-
-        CollectionReference collection = firestore.collection("ScreenTimeOCRResult");
-
-        ApiFuture<QuerySnapshot> apiFutureList = collection
-                .whereEqualTo("user_id", user_id)
-                .whereEqualTo("date", targetDate)
-                .get();
-
-        QuerySnapshot querySnapshot = apiFutureList.get();
-
-        if(!querySnapshot.isEmpty()){
-            return querySnapshot.getDocuments().get(0).toObject(ScreenTimeOCRResult.class);
-        }else{
-            return null;
-        }*/
         Map<String, Object> filters = Map.of(
                 "user_id", user_id,
                 "date", targetDate
