721560aec83f0b585c85d58128e6b3f7977a83a7
정하은 <he3080@naver.com>
2025-01-13 19:26:52 +0900

    Refact: 필요없는 주석 제거

--- src/main/java/com/example/iplan/Repository/SetScreenTimeRepository.java
+++ src/main/java/com/example/iplan/Repository/SetScreenTimeRepository.java
@@ -2,14 +2,9 @@ package com.example.iplan.Repository;
 
 import com.example.iplan.Domain.ScreenTime;
 import com.example.iplan.Repository.DefaultFirebaseRepository.DefaultFirebaseDBRepository;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QuerySnapshot;
-import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -23,22 +18,6 @@ public class SetScreenTimeRepository extends DefaultFirebaseDBRepository<ScreenT
     }
 
     public ScreenTime findByDate(String user_id, String targetDate) throws ExecutionException, InterruptedException{
-        /*Firestore firestore = FirestoreClient.getFirestore();
-
-        CollectionReference collection = firestore.collection("ScreenTime");
-
-        ApiFuture<QuerySnapshot> apiFutureList = collection
-                .whereEqualTo("user_id", user_id)
-                .whereEqualTo("date", targetDate)
-                .get();
-
-        QuerySnapshot querySnapshot = apiFutureList.get();
-
-        if(!querySnapshot.isEmpty()){
-            return querySnapshot.getDocuments().get(0).toObject(ScreenTime.class);
-        }else{
-            return null;
-        }*/
         Map<String, Object> filters = Map.of(
                 "user_id", user_id,
                 "date", targetDate
