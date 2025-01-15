fb54da616011e4f46ceabc319024ec016509fd9a
정하은 <he3080@naver.com>
2025-01-15 22:45:54 +0900

    Feat: CalendarController Swagger 처리

--- src/main/java/com/example/iplan/Controller/CalendarController.java
+++ src/main/java/com/example/iplan/Controller/CalendarController.java
@@ -1,6 +1,14 @@
 package com.example.iplan.Controller;
 
+import com.example.iplan.Domain.DayData;
 import com.example.iplan.Service.CalendarService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.checkerframework.checker.units.qual.A;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +24,7 @@ import java.util.concurrent.ExecutionException;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/calendar")
+@Tag(name = "달력 페이지 컨트롤러", description = "선택한 한달 간의 데이터를 처리, 관리한다.")
 public class CalendarController {
 
     private final CalendarService calendarService;
@@ -28,9 +37,16 @@ public class CalendarController {
      * @throws ExecutionException
      * @throws InterruptedException
      */
+    @Operation(summary = "한 달간 보상 지급 완료 (success = true) 된 데이터 리스트 GET", description = "해당 년/월에 해당하는 하루 데이터에서 보상 지급 완료된 월/일, 도장 여부 데이터를 가져온다.",
+            parameters = {
+                    @Parameter(name = "yearMonth", description = "원하는 년/월", example = "2025-01", required = true)
+            })
     @GetMapping("/{yearMonth}")
-    public ResponseEntity<Map<String, Object>> getMonthCalendarData(@PathVariable String yearMonth, @AuthenticationPrincipal String userId) throws ExecutionException, InterruptedException {
+    public ResponseEntity<Map<String, Object>> getMonthCalendarData
+    (@PathVariable @Parameter(description = "원하는 년/월", example = "2025-01") String yearMonth, @AuthenticationPrincipal String userId) {
+
         return calendarService.getAllCalendarData(yearMonth, userId);
+
     }
 
     /**
@@ -41,8 +57,16 @@ public class CalendarController {
      * @throws ExecutionException
      * @throws InterruptedException
      */
+    @Operation(summary = "해당 날짜의 계획 진행 데이터 모두 가져오기", description = "달력에서 특정 날짜 클릭시 해당 날의 계획 진행 정도를 보여 준다.",
+            parameters = {
+                    @Parameter(name = "yearMonthDate", description = "원하는 년/월/일", example = "2025-01-22", required = true)
+            })
+    @ApiResponses(value = {
+            @ApiResponse(content = @Content(schema = @Schema(implementation = DayData.class))),
+    })
     @GetMapping("/showTargetDateData/{yearMonthDate}")
-    public ResponseEntity<Map<String, Object>> getTargetDateData(@PathVariable String yearMonthDate, @AuthenticationPrincipal String userId) throws ExecutionException, InterruptedException {
+    public ResponseEntity<Map<String, Object>> getTargetDateData
+    (@PathVariable @Parameter(description = "원하는 년/월/일", example = "2025-01-22") String yearMonthDate, @AuthenticationPrincipal String userId) throws ExecutionException, InterruptedException {
         return calendarService.getTargetDateData(yearMonthDate, userId);
     }
 }
