8233847c4d57c1ef9a15e03b9abb7280eafb7501
정하은 <he3080@naver.com>
2025-01-15 22:51:02 +0900

    Feat: 엔티티, DTO Swagger 어노테이션 처리

--- src/main/java/com/example/iplan/Domain/DayData.java
+++ src/main/java/com/example/iplan/Domain/DayData.java
@@ -3,6 +3,7 @@ package com.example.iplan.Domain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.google.cloud.firestore.annotation.DocumentId;
 import com.google.firebase.database.annotations.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
@@ -17,28 +18,38 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "하루 계획의 모든 정보 데이터를 담은 엔티티")
 public class DayData {
 
+    @Schema(description = "하루 계획 데이터의 고유 ID", example = "12345", requiredMode = Schema.RequiredMode.REQUIRED)
     @DocumentId
     private String id;
 
+    @Schema(description = "유저 ID", example = "1user2345", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull
     private String user_id;
 
+    @Schema(description = "해당 날의 년도", example = "2025", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonFormat(pattern = "yyyy")
+    @NotNull
     private String year;
 
+    @Schema(description = "해당 날의 월", example = "01", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonFormat(pattern = "MM")
+    @NotNull
     private String month;
 
+    @Schema(description = "해당 날", example = "22", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonFormat(pattern = "dd")
+    @NotNull
     private String date;
 
-    private boolean isReachGoal;
-
+    @Schema(description = "해당 날에 있는 계획 ID 리스트", example = "[\"df-xc123\", \"34nXd@\"]")
     private List<String> plan_idList;
 
+    @Schema(description = "해당 날의 스크린 타임 데이터 Id", example = "screenTime3294-xd")
     private String screenTime_id;
 
-    private String reward_id;
+    @Schema(description = "해당 날 보상 지급 여부", example = "false")
+    private Boolean is_rewarded;
 }
--- src/main/java/com/example/iplan/Domain/ScreenTime.java
+++ src/main/java/com/example/iplan/Domain/ScreenTime.java
@@ -3,10 +3,8 @@ package com.example.iplan.Domain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.google.cloud.firestore.annotation.DocumentId;
 import com.google.firebase.database.annotations.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -18,21 +16,33 @@ import java.time.LocalTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Schema(description = "하루의 스크린타임 목표 데이터를 담은 엔티티")
 public class ScreenTime {
+
+    @Schema(description = "스크린타임 데이터 고유 Id", example = "sdfi2-zx4", requiredMode = Schema.RequiredMode.REQUIRED)
     @DocumentId
     private String id;
 
+    @Schema(description = "사용자 ID", example = "user123", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull
     private String user_id;
 
+    @Schema(description = "스크린타임 목표 설정된 날짜", example = "2025-01-22", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotNull
     private String date;
 
     // 사용자가 정한 최소 측정 마감 시간
+    @Schema(description = "사용자가 해당 시간 까지 측정된 스크린 타임으로 측정하겠다 하는 목표 마감 시간(--시--분), 이 시간 이후로 캡쳐된 사진만 업로드 허용",
+            example = "20:30", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonFormat(pattern = "HH:mm")
+    @NotNull
     private String deadLineTime;
 
     // 사용자가 정한 핸드폰 사용 시간
+    @Schema(description = "사용자가 정한 핸드폰 사용 시간(-시간-분)", example = "05:00", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonFormat(pattern = "HH:mm")
+    @NotNull
     private String goalTime;
 }
