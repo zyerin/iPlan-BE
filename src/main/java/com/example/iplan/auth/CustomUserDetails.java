7745f830bfdb9580644dcbb3d999a1a2a594c7da
zyerin <zyerin5974@gmail.com>
2025-02-06 22:05:10 +0900

    feat: CustomUserDetails 객체 생성

--- src/main/java/com/example/iplan/auth/CustomUserDetails.java
+++ src/main/java/com/example/iplan/auth/CustomUserDetails.java
@@ -1,2 +1,50 @@
-package com.example.iplan.Auth;public class CustomUserDetails {
+package com.example.iplan.auth;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class CustomUserDetails implements UserDetails {
+
+    private String username;  // email
+
+    private String password;
+
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.getRole()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
