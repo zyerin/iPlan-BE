1ecf4825e84b5df03d152649406406fbaac7dd7a
zyerin <zyerin5974@gmail.com>
2025-02-06 22:45:57 +0900

    feat: UserRole 생성하여 사용자 권한을 Enum으로 관리

--- src/main/java/com/example/iplan/auth/UserRole.java
+++ src/main/java/com/example/iplan/auth/UserRole.java
@@ -1,2 +1,24 @@
-package com.example.iplan.auth;public class UserRole {
+package com.example.iplan.auth;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    CHILD("ROLE_CHILD"),
+    PARENT("ROLE_PARENT");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public static UserRole fromString(String role) {
+        for (UserRole userRole : UserRole.values()) {
+            if (userRole.name().equalsIgnoreCase(role)) {
+                return userRole;
+            }
+        }
+        throw new IllegalArgumentException("Unknown role: " + role);
+    }
 }
