6bdcfca850816020922cee36f9ba9f5823788c96
zyerin <zyerin5974@gmail.com>
2025-02-06 22:44:50 +0900

    feat: UserRepository 생성

--- src/main/java/com/example/iplan/auth/UserRepository.java
+++ src/main/java/com/example/iplan/auth/UserRepository.java
@@ -4,12 +4,35 @@ import com.example.iplan.Repository.DefaultFirebaseRepository.DefaultFirebaseDBR
 import com.google.cloud.firestore.Firestore;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
 @Repository
-public class UsersRepository extends DefaultFirebaseDBRepository<Users>{
-    public UsersRepository(Firestore firestore)
-    {
+public class UserRepository extends DefaultFirebaseDBRepository<Users> {
+    public UserRepository(Firestore firestore) {
         super(firestore);
         setEntityClass(Users.class);
         setCollectionName("User");
     }
+
+    public Optional<Users> findByEmail(String email) {
+        try {
+            Users user = findByFields(Map.of("email", email)); // 이메일을 기반으로 조회
+            return Optional.ofNullable(user);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Users> findById(String userId) {
+        try {
+            return Optional.ofNullable(findEntityByDocumentId(userId));
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
 }
