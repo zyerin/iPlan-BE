628f8e535476b6a356dc5bf0c61cbfcc4e69e0d4
zyerin <zyerin5974@gmail.com>
2025-02-06 22:09:21 +0900

    feat: 로그인, 회원가입 컨트롤러 생성

--- src/main/java/com/example/iplan/auth/UserController.java
+++ src/main/java/com/example/iplan/auth/UserController.java
@@ -1,2 +1,43 @@
-package com.example.iplan.auth;public class UserController {
+package com.example.iplan.auth;
+
+import com.example.iplan.config.jwt.JwtToken;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/auth")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    @Operation(summary = "회원가입")
+    public ResponseEntity<String> signUp(@RequestBody SignUpDTO signUpDto){
+        try {
+            String result = userService.signUp(signUpDto.getEmail(), signUpDto.getPassword(), signUpDto.getName(), signUpDto.getAuthority());
+            return ResponseEntity.ok(result);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인")
+    public JwtToken signIn(@RequestBody SignInDTO signInDto) {
+        String email = signInDto.getEmail();
+        String password = signInDto.getPassword();
+        String fcmToken = signInDto.getFcmToken();
+        log.info("Login request: email = {}, password = {}, fcmToken = {}", email, password, fcmToken);
+
+        JwtToken jwtToken = userService.signIn(email, password, fcmToken);
+        log.info("JwtToken accessToken = {}, refreshToken = {}", jwtToken.getAccessToken(), jwtToken.getRefreshToken());
+        return jwtToken;
+    }
 }
