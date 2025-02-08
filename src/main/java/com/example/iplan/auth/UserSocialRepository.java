96e310851252cabdfda4e351a741ae5815c2801b
zyerin <zyerin5974@gmail.com>
2025-02-08 15:14:40 +0900

    refact: 파일 위치 이동

--- src/main/java/com/example/iplan/auth/UserSocialRepository.java
+++ src/main/java/com/example/iplan/auth/UserSocialRepository.java
@@ -1,8 +1,7 @@
-package com.example.iplan.Repository;
+package com.example.iplan.auth;
 
 // 유저 정보를 Firestore 에 저장하기 위한 리포지토리
 import com.example.iplan.Repository.DefaultFirebaseRepository.DefaultFirebaseDBRepository;
-import com.example.iplan.Domain.UserSocial;
 import org.springframework.stereotype.Repository;
 import com.google.cloud.firestore.Firestore;
 
